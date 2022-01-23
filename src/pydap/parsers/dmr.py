222c99f9a158d5d008a1762a75be0f9eda876d63
Edward Hartnett <Edward.Hartnett@noaa.gov>
2022-01-23 08:06:54 -0700

    development of dap4 parsing

--- src/pydap/parsers/dmr.py
+++ src/pydap/parsers/dmr.py
@@ -9,21 +9,20 @@ from xml.dom import minidom
 from ..model import (DatasetType, BaseType,
                      SequenceType, StructureType,
                      GridType)
-from ..lib import (quote, LOWER_DAP2_TO_NUMPY_PARSER_TYPEMAP)
+from ..lib import (quote, DAP4_TO_NUMPY_PARSER_TYPEMAP)
 
 constructors = ('grid', 'sequence', 'structure')
 name_regexp = r'[\w%!~"\'\*-]+'
 
 
-# def DAP2_parser_typemap(type_string):
-#     """
-#     This function takes a numpy dtype object
-#     and returns a dtype object that is compatible with
-#     the DAP2 specification.
-#     """
-#     dtype_str = LOWER_DAP2_TO_NUMPY_PARSER_TYPEMAP[type_string.lower()]
-#     return np.dtype(dtype_str)
-
+def DAP4_parser_typemap(type_string):
+    """
+    This function takes a numpy dtype object
+    and returns a dtype object that is compatible with
+    the DAP2 specification.
+    """
+    dtype_str = DAP4_TO_NUMPY_PARSER_TYPEMAP[type_string]
+    return np.dtype(dtype_str)
 
 class DMRParser():
 
@@ -31,12 +30,6 @@ class DMRParser():
 
     def __init__(self, dmr):
         self.dmr = dmr
-
-    # def consume(self, regexp):
-    #     """Consume and return a token."""
-    #     token = super(DMRParser, self).consume(regexp)
-    #     self.buffer = self.buffer.lstrip()
-    #     return token
 
     # def parse(self):
     #     """Parse the DAS, returning a dataset."""
@@ -154,8 +147,55 @@ class DMRParser():
 
 def build_dataset(dmr):
     """Return a dataset object from a DMR representation."""
-    print(dmr)
-    return minidom.parseString(dmr)
+
+    # Parse the DMR.
+#    print(dmr)
+    doc = minidom.parseString(dmr)
+#    import pdb; pdb.set_trace()
+
+    # Print out the metadata.
+    dsizes = {}
+    for dim in doc.getElementsByTagName("Dimension"):
+        print("Dimension " + dim.getAttribute("name") + " size " + dim.getAttribute("size"))
+        dsizes[dim.getAttribute("name")] = dim.getAttribute("size")
+    for att in doc.getElementsByTagName("Attribute"):        
+        print("\tAtt name " + att.getAttribute("name") + " type " + att.getAttribute("type"))
+    for float64 in doc.getElementsByTagName("Float64"):
+        print("Float64 var name " + float64.getAttribute("name"))
+        for var_dim in float64.getElementsByTagName("Dim"):
+            print("\tDim name " + var_dim.getAttribute("name") + " size " + var_dim.getAttribute("size"))
+            for var_att in float64.getElementsByTagName("Attribute"):
+                print("\tAtt name " + var_att.getAttribute("name") + " type " + var_att.getAttribute("type"))
+
+    # Create and fill in the pydap data model.
+    dataset = DatasetType('nameless')
+
+    # Handle each variable of atomic type.
+    dmr_atomic_types = ('Int8', 'UInt8', 'Byte', 'Char', 'Int16', 'UInt16', 'Int32', 'UInt32',
+                        'Int64', 'UInt64', 'Float32', 'Float64')
+    for atomic_type in dmr_atomic_types:
+        for dmr_var in doc.getElementsByTagName(atomic_type):
+            print("Handling var " + dmr_var.getAttribute("name"))
+            grid = GridType('nameless')
+            name = dmr_var.getAttribute("name")
+            dimensions = []
+            shape = []
+            for var_dim in dmr_var.getElementsByTagName("Dim"):
+                var_dim_name = var_dim.getAttribute("name")[var_dim.getAttribute("name").rindex("/") + 1:]
+                dimensions.append(var_dim_name)
+                shape.append(dsizes[var_dim_name])
+            parser_dtype = DAP4_parser_typemap(atomic_type)
+            data = DummyData(parser_dtype, shape)
+            var = BaseType(name, data, dimensions)
+            for var_att in dmr_var.getElementsByTagName("Attribute"):
+                var.attribute[var_att.getAttribute("name")] = {}
+            dataset[var.name] = var            
+
+#    ints = doc.getElementsByTagName("Int32")
+#    var = BaseType(ints[0].getAttribute("name"))
+#    dataset[var.name] = var
+
+    return dataset
 
 
 class DummyData(object):
--- src/pydap/tests/test_parsers_dap4.py
+++ src/pydap/tests/test_parsers_dap4.py
@@ -2,54 +2,254 @@
 
 import numpy as np
 from pydap.parsers.das import add_attributes, parse_das
-from pydap.parsers.dds import build_dataset
+#from pydap.parsers.dds import build_dataset
 from pydap.parsers.dmr import build_dataset
 from pydap.tests.test_parsers_dds import DDS
 import unittest
 
-DMR = """<Dataset name="foo">
+# Examples are taken from https://docs.opendap.org/index.php/DAP4:_Specification_Volume_1.
+DMR_single_scalar = """<Dataset name="foo">
 <Int32 name="x"/>
 </Dataset>
-}"""
+"""
+
+DMR_coads_climatology2 = """<Dataset xmlns="http://xml.opendap.org/ns/DAP/4.0#" xml:base="http://test.opendap.org:8080/opendap/hyrax/data/nc/coads_climatology2.nc" dapVersion="4.0" dmrVersion="1.0" name="coads_climatology2.nc">
+<Dimension name="COADSX" size="180"/>
+<Dimension name="COADSY" size="90"/>
+<Dimension name="TIME" size="12"/>
+<Float64 name="COADSX">
+<Dim name="/COADSX"/>
+<Attribute name="units" type="String">
+<Value>degrees_east</Value>
+</Attribute>
+<Attribute name="modulo" type="String">
+<Value> </Value>
+</Attribute>
+<Attribute name="point_spacing" type="String">
+<Value>even</Value>
+</Attribute>
+</Float64>
+<Float64 name="COADSY">
+<Dim name="/COADSY"/>
+<Attribute name="units" type="String">
+<Value>degrees_north</Value>
+</Attribute>
+<Attribute name="point_spacing" type="String">
+<Value>even</Value>
+</Attribute>
+</Float64>
+<Float64 name="TIME">
+<Dim name="/TIME"/>
+<Attribute name="units" type="String">
+<Value>hour since 0000-01-01 00:00:00</Value>
+</Attribute>
+<Attribute name="time_origin" type="String">
+<Value>1-JAN-0000 00:00:00</Value>
+</Attribute>
+<Attribute name="modulo" type="String">
+<Value> </Value>
+</Attribute>
+</Float64>
+<Float32 name="SST">
+<Dim name="/TIME"/>
+<Dim name="/COADSY"/>
+<Dim name="/COADSX"/>
+<Attribute name="missing_value" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="_FillValue" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="long_name" type="String">
+<Value>SEA SURFACE TEMPERATURE</Value>
+</Attribute>
+<Attribute name="history" type="String">
+<Value>From coads_climatology</Value>
+</Attribute>
+<Attribute name="units" type="String">
+<Value>Deg C</Value>
+</Attribute>
+<Map name="/TIME"/>
+<Map name="/COADSY"/>
+<Map name="/COADSX"/>
+</Float32>
+<Float32 name="AIRT">
+<Dim name="/TIME"/>
+<Dim name="/COADSY"/>
+<Dim name="/COADSX"/>
+<Attribute name="missing_value" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="_FillValue" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="long_name" type="String">
+<Value>AIR TEMPERATURE</Value>
+</Attribute>
+<Attribute name="history" type="String">
+<Value>From coads_climatology</Value>
+</Attribute>
+<Attribute name="units" type="String">
+<Value>DEG C</Value>
+</Attribute>
+<Map name="/TIME"/>
+<Map name="/COADSY"/>
+<Map name="/COADSX"/>
+</Float32>
+<Float32 name="SPEH">
+<Dim name="/TIME"/>
+<Dim name="/COADSY"/>
+<Dim name="/COADSX"/>
+<Attribute name="missing_value" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="_FillValue" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="long_name" type="String">
+<Value>SPECIFIC HUMIDITY</Value>
+</Attribute>
+<Attribute name="history" type="String">
+<Value>From coads_climatology</Value>
+</Attribute>
+<Attribute name="units" type="String">
+<Value>G/KG</Value>
+</Attribute>
+<Map name="/TIME"/>
+<Map name="/COADSY"/>
+<Map name="/COADSX"/>
+</Float32>
+<Float32 name="WSPD">
+<Dim name="/TIME"/>
+<Dim name="/COADSY"/>
+<Dim name="/COADSX"/>
+<Attribute name="missing_value" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="_FillValue" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="long_name" type="String">
+<Value>WIND SPEED</Value>
+</Attribute>
+<Attribute name="history" type="String">
+<Value>From coads_climatology</Value>
+</Attribute>
+<Attribute name="units" type="String">
+<Value>M/S</Value>
+</Attribute>
+<Map name="/TIME"/>
+<Map name="/COADSY"/>
+<Map name="/COADSX"/>
+</Float32>
+<Float32 name="UWND">
+<Dim name="/TIME"/>
+<Dim name="/COADSY"/>
+<Dim name="/COADSX"/>
+<Attribute name="missing_value" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="_FillValue" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="long_name" type="String">
+<Value>ZONAL WIND</Value>
+</Attribute>
+<Attribute name="history" type="String">
+<Value>From coads_climatology</Value>
+</Attribute>
+<Attribute name="units" type="String">
+<Value>M/S</Value>
+</Attribute>
+<Map name="/TIME"/>
+<Map name="/COADSY"/>
+<Map name="/COADSX"/>
+</Float32>
+<Float32 name="VWND">
+<Dim name="/TIME"/>
+<Dim name="/COADSY"/>
+<Dim name="/COADSX"/>
+<Attribute name="missing_value" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="_FillValue" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="long_name" type="String">
+<Value>MERIDIONAL WIND</Value>
+</Attribute>
+<Attribute name="history" type="String">
+<Value>From coads_climatology</Value>
+</Attribute>
+<Attribute name="units" type="String">
+<Value>M/S</Value>
+</Attribute>
+<Map name="/TIME"/>
+<Map name="/COADSY"/>
+<Map name="/COADSX"/>
+</Float32>
+<Float32 name="SLP">
+<Dim name="/TIME"/>
+<Dim name="/COADSY"/>
+<Dim name="/COADSX"/>
+<Attribute name="missing_value" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="_FillValue" type="Float32">
+<Value>-9.99999979e+33</Value>
+</Attribute>
+<Attribute name="long_name" type="String">
+<Value>SEA LEVEL PRESSURE</Value>
+</Attribute>
+<Attribute name="history" type="String">
+<Value>From coads_climatology</Value>
+</Attribute>
+<Attribute name="units" type="String">
+<Value>MB</Value>
+</Attribute>
+<Map name="/TIME"/>
+<Map name="/COADSY"/>
+<Map name="/COADSX"/>
+</Float32>
+<Attribute name="NC_GLOBAL" type="Container">
+<Attribute name="history" type="String">
+<Value>FERRET V5.22 11-Jan-01</Value>
+</Attribute>
+</Attribute>
+<Attribute name="DODS_EXTRA" type="Container">
+<Attribute name="Unlimited_Dimension" type="String">
+<Value>TIME</Value>
+</Attribute>
+</Attribute>
+</Dataset>
+"""
 
 # It is important to add attributes that have the same
 # name as the dimensions of SPEH. This is an edge
 # case that can break the das parser.
 
-
 class TestParseDMR(unittest.TestCase):
 
     """Test DMR parser."""
 
-    def setUp(self):
-        """Load a dataset and apply DAS to it."""
-        self.dataset = build_dataset(DDS)
-        attributes = parse_das(DAS)
-        add_attributes(self.dataset, attributes)
+#    def setUp(self):
+#        """Load a dataset and apply DAS to it."""
+#        attributes = parse_das(DMR)
+#        add_attributes(self.dataset, attributes)
 
-#   def test_basic(self):
-#        """Test a basic attribute."""
-#        self.assertEqual(self.dataset.structure.b.value, 1)
+    def test_single_scalar(self):
+        """Test a single scalar case."""
+        self.dataset = build_dataset(DMR_single_scalar)
+#        import pdb; pdb.set_trace()
+        self.assertEqual(self.dataset.attributes, {})
+        self.assertEqual(self.dataset["x"].name, 'x')
 
-    # def test_nan(self):
-    #     """Test NaN."""
-    #     self.assertTrue(np.isnan(self.dataset.structure.b.missing))
+    def test_coads_climatology2(self):
+        """Test a single scalar case."""
+        self.dataset = build_dataset(DMR_coads_climatology2)
+        for v in self.dataset:
+            print("dataset has " + v)
+#        self.assertEqual(self.dataset.attributes, {})
+#        self.assertEqual(self.dataset["x"].name, 'x')
 
-    # def test_multiple_values(self):
-    #     """Test attributes with multiple values."""
-    #     self.assertEqual(self.dataset.structure.b.foo, ["one", "two"])
 
-    # def test_dot_attribute(self):
-    #     """Test dotted attributes."""
-    #     self.assertEqual(self.dataset.structure.i.answer, 42)
-
-    # def test_meta_attributes(self):
-    #     """Test attributes not associated with any variables."""
-    #     self.assertEqual(self.dataset.meta, {"debug": "1"})
-
-    # def test_SPEH_attributes(self):
-    #     """Test attributes not associated with any variables."""
-    #     self.assertEqual(self.dataset.SPEH.debug, 1)
-    #     self.assertEqual(self.dataset.SPEH.attributes['TIME'], 0)
-    #     self.assertEqual(self.dataset.SPEH.attributes['COADSX'], 1e20)
-    #     self.assertEqual(self.dataset.SPEH.attributes['COADSY'], "zero")
